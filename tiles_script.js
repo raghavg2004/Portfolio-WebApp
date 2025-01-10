ac7df0b1cce03122b22dcfc24be5eeab3eaa4825
尺卂Ꮆ卄卂ᐯ   Ꮆㄩ卩ㄒ卂 <142708034+raghavg2004@users.noreply.github.com>
2025-01-10 22:59:22 +0530

    Update tiles_script.js

--- tiles_script.js
+++ tiles_script.js
@@ -7,7 +7,7 @@ const colors = [
 const images = [
     
     './assets/image13.jpg', './assets/image15.jpg', './assets/image9.jpg', './assets/image3.jpg',
-    './assets/image5.jpg', './assets/image10.jpg', './assets/image11.jpg', './assets/image14.jpg',
+    './assets/image5.jpg', './assets/image17.jpg', './assets/image11.jpg', './assets/image14.jpg',
     './assets/image1.jpg', './assets/image2.jpg', './assets/image8.jpg', './assets/image4.jpg',
     './assets/image7.jpg', './assets/image12.jpg', './assets/image6.jpg', './assets/image16.jpg'
 ];
